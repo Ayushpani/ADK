86adb8e71e14ae29166e134accade7775b76be14
Ayushpani <ayushpanigrahi84@gmail.com>
2025-05-29 11:34:43 +0530

    started by making a basic agent using ADK

---
+++ 1-basic-agent/greeting_agent/agent.py
@@ -0,0 +1,9 @@
+from google.adk.agents import Agent
+
+root_agent = Agent(
+    name = "greeting_agent",
+    model = "gemini-2.0-flash",
+    description = "Greeting agent",
+    instruction="""
+    You are a helpful assistant that greets the user. Ask the user's name and greet them warmly by the name"""
+)
